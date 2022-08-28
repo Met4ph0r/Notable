62f7b5f7488f0832aebaec175142a9666302fc0d
Hrusikesh Kumar <108739091+Met4ph0r@users.noreply.github.com>
2022-08-28 13:05:53 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        background: "#cff9ff22",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright (2022) &copy; Hrusikesh Kumar</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./LandingStyles.css";
+
+function LandingPage({ history }) {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
+
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title1">Welcome to</h1>
+              <h1 className="title2">Notable</h1>
+              <p className="subtitle">A place for all your ideas.</p>
+            </div>
+            <div className="buttonContainer">
+              <Link to="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </Link>
+              <Link to="/register">
+                <Button
+                  variant="outline-primary"
+                  size="lg"
+                  className="landingbutton signup"
+                >
+                  Signup
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default LandingPage;
